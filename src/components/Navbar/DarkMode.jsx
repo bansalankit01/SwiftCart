ed9e5ddb166fca1a2e2253cc94a578ac724f36c6
ANKIT <bansalankit8447@gmail.com>
2025-02-11 13:09:38 +0530

    testing

--- src/components/Navbar/DarkMode.jsx
+++ src/components/Navbar/DarkMode.jsx
@@ -1,12 +1,12 @@
 import  { useEffect, useState } from 'react';
-import  LightButton from "../../assets/website/light-photoroom.png";
-import DarkButton from "../../assets/website/dark-photoroom.png";
+// import  DarkButton from "../../../public/light-photoroom.png";
+// import LightButton from "../../../public/dark-photoroom.png";
 
 function DarkMode() {
 
 
     const [theme, setTheme] = useState(
-        localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
+        localStorage.getItem("theme") ? localStorage.getItem("theme") : "dark"
     );
 
     const element = document.documentElement;
@@ -26,15 +26,15 @@ function DarkMode() {
   return (
 
     <div className='relative'> 
-        <img src={LightButton} alt="Light Button" onClick={() => {
+        <img src="https://static-00.iconduck.com/assets.00/mode-light-icon-2048x2048-n3gewm0l.png"  alt="Light" onClick={() => {
             setTheme(theme == "light" ? "dark" : "light")
         }} 
-         className={`w-12 cursor-pointer drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] transition-all duration-300 absolute right-0 z-10 ${theme == "dark" ? "opacity-0": "opacity-100"}`}
+         className={`w-8 cursor-pointer drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] transition-all duration-300 absolute right-0 z-10 ${theme == "dark" ? "opacity-0": "opacity-100"}`}
         />
-        <img src={DarkButton} alt="Dark Button" onClick={() => {
+        <img src="https://cdn0.iconfinder.com/data/icons/multimedia-solid-30px/30/sun_light_mode_day-512.png" alt="Dark" onClick={() => {
             setTheme(theme == "light" ? "dark" : "light")
         }} 
-        className='w-12 cursor-pointer drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] transition-all duration-300' 
+        className='w-8 cursor-pointer drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] transition-all duration-300' 
          />
     </div>
 
